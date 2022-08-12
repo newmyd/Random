f3626d5c91b8beeb96bec063fefcc7a39b6d270e
newmyd <95059595+newmyd@users.noreply.github.com>
2022-08-12 16:31:24 +0800

    Add files via upload

---
+++ random.js
@@ -0,0 +1,15 @@
+function rand(n) {
+	return arguments.length == 1 ? Math.floor(Math.random() * n) : 0;
+}
+function random(l, r) {
+	return arguments.length == 2 ? rand(r - l + 1) + l : 0;
+}
+
+var num = document.getElementById("num");
+
+var l = 1,
+    r = 48;
+
+function onClick() {
+    num.innerHTML = random(l, r);
+}
